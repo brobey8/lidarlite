30d4add055925e6a2fdc9fd55889867412f00383
ratbag <ben.robey@vtron.com.au>
2017-12-12 19:09:15 +1000

    Moving from embd to a lighter i2c implementation
    embd wasn't working properly

--- main.go
+++ main.go
@@ -1,69 +1,52 @@
-package lidarlite       
+package lidarlite
 
-import (                                                                                                                          
-        "github.com/kidoman/embd"                                                                                                 
-        _ "github.com/kidoman/embd/host/rpi" // This loads the RPi driver                                                         
-)                                                                                                                                 
+import (
+	"github.com/d2r2/go-i2c"
+	"log"
+	//"os"
+)
 
-const (                                                                                                                           
-        address = 0x62     
+const (
+	address = 0x62
 
-        kLidarLiteCommandControlRegister          = 0x00                                                                          
-        kLidarLiteVelocityMeasurementOutput               = 0x09                                                                  
-        kLidarLiteCalculateDistanceMSB                    = 0x8f                                                                  
-        kLidarLiteCalculateDistanceLSB                    = 0x10                                                                  
-        kLidarLitePreviousMeasuredDistanceMSB             = 0x94                                                                  
-        kLidarLitePreviousMeasuredDistanceLSB             = 0x15                                                                  
-        kLidarLiteHardwareVersion                          = 0x41                                                                 
-        kLidarLiteSoftwareVersion                          = 0x4f                                                                 
-        kLidarLiteMeasure                                  = 0x04                                                                 
-		
-        pollDelay = 250                                                                                                           
-)      
+	kLidarLiteCommandControlRegister      = 0x00
+	kLidarLiteVelocityMeasurementOutput   = 0x09
+	kLidarLiteCalculateDistanceMSB        = 0x8f
+	kLidarLiteCalculateDistanceLSB        = 0x10
+	kLidarLitePreviousMeasuredDistanceMSB = 0x94
+	kLidarLitePreviousMeasuredDistanceLSB = 0x15
+	kLidarLiteHardwareVersion             = 0x41
+	kLidarLiteSoftwareVersion             = 0x4f
+	kLidarLiteMeasure                     = 0x04
+)
 
-// BMP085 represents a LIDAR-Lite v2 sensor.
 type LIDARLITE struct {
-	Bus  embd.I2CBus
-	Poll int
-}
-  
-
-// New returns a handle to a LidarLite sensor.
-// https://static.garmin.com/pumac/LIDAR_Lite_v3_Operation_Manual_and_Technical_Specifications.pdf
-func New(bus embd.I2CBus) *LIDARLITE {
-	return &LIDARLITE{Bus: bus, Poll: pollDelay}
+	Bus *i2c.I2C
 }
 
+// represents a LIDAR-Lite v2 sensor.
+func New(bus *i2c.I2C) *LIDARLITE {
+	return &LIDARLITE{Bus: bus}
+}
 
-func (d *LIDARLITE) ReadDistance() (byte, error) {
-	//Write 0x04 to register 0x00
-	//Read register 0x01. Repeat until bit 0 (LSB) goes low
-	//Read two bytes from 0x8f (High byte 0x0f then low byte 0x10) to obtain the 16-bit measured distance in cm
-	err := d.Bus.WriteByteToReg(address, kLidarLiteCommandControlRegister, kLidarLiteMeasure)
+func (d *LIDARLITE) ReadDistance() int {
+	//Read the distance. Needs to return an error and not log it. Should not have a log dependancy
+	err := d.Bus.WriteRegU8(kLidarLiteCommandControlRegister, kLidarLiteMeasure)
 	if err != nil {
-		return 0, err
+		log.Fatal(err)
 	}
-	msb, err := d.Bus.ReadByteFromReg(address, kLidarLiteCalculateDistanceMSB)
+
+	msb, err := d.Bus.ReadRegU8(kLidarLitePreviousMeasuredDistanceMSB)
 	if err != nil {
-		return 0, err
+		log.Fatal(err)
 	}
-	lsb, err := d.Bus.ReadByteFromReg(address, kLidarLiteCalculateDistanceLSB)
+
+	lsb, err := d.Bus.ReadRegU8(kLidarLitePreviousMeasuredDistanceLSB)
 	if err != nil {
-		return 0, err
+		log.Fatal(err)
 	}
+
 	distance := (msb << 8) + lsb
-	
-	
-	return distance, nil
+
+	return int(distance)
 }
-
-func (d *LIDARLITE) SoftwareVersion() (byte, error) {
-	version, err := d.Bus.ReadByteFromReg(address, kLidarLiteHardwareVersion)
-	if err != nil {
-		return 0, err
-	}
-	return version, nil
-}	
-
-
-
